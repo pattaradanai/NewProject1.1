638dbbb9093a2277e2bbc79d9a5704ba9ddd9252
pattaradanai <billlove.sick@hotmail.com>
2018-04-17 12:01:33 +0700

    index

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Shift &mdash; Free Website Template, Free HTML5 Template by FreeHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!--  Object  -->
+	<script src='http://code.jquery.com/jquery-1.9.1.min.js' type='text/javascript'></script>
+<script src='jquery.reel.js' type='text/javascript'></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="fh5co-top-logo">
+				<div id="fh5co-logo"><a href="Login.html">Login</a></div>
+			</div>
+			<div class="fh5co-top-menu menu-1 text-center">
+				<ul>
+					<	<!-- Link Menu Write here (Web)-->
+					<li class="has-dropdown">
+						
+						<ul class="dropdown">
+							<!-- Link Menu Write here (mobile)-->
+						</ul>
+					</li>
+					
+				</ul>
+			</div>
+			<div class="fh5co-top-social menu-1 text-right">
+				<ul class="fh5co-social">
+					
+					<li><a href="https://github.com/pattaradanai/NewProject1.1"><i class="icon-github"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<div id="fh5co-work">
+		<div class="container">
+			<div class="row top-line animate-box">
+				<div class="col-md-7 col-md-push-5 text-left intro">
+					<h2> Welcome To PortFolio Student  <i class="icon-heart2"></i> </h2>
+					<!-- <h2>Shift is a Collection of a Beautiful &amp; Premium Themes.</h2> -->
+				</div>
+				
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-color: white">
+							
+								<div class="desc" align="center">
+									<?php include 'Object.html'; ?>
+								</div>
+							
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-2.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Power Led Light</h3>
+									<span class="cat">Illustration</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-3.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Elephant Origami</h3>
+									<span class="cat">Logo</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-4.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Electric Kick Scooter</h3>
+									<span class="cat">Web Design</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-5.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Shark Diver</h3>
+									<span class="cat">Branding</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-6.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Objekte Unserer Tage Berlin</h3>
+									<span class="cat">Logo</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-7.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Aloha Shirt</h3>
+									<span class="cat">Web Design</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-8.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Olafur Elliasson Waterfall Versailles</h3>
+									<span class="cat">Branding</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="portfolio_detail.html">
+						<div class="work-grid" style="background-image:url(images/project-9.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Hand Care</h3>
+									<span class="cat">Logo</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<footer id="fh5co-footer" role="contentinfo">
+
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<!-- Object -->
+	<script> $.reel.def.indicator = 5; </script>
+
+	</body>
+</html>
+
