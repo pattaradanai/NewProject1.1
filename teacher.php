0b0f3bfad0e5d16db8f6180c48059d294880c8aa
Patiharn Pukdee <venomfluk@gmail.com>
2018-07-13 13:39:35 +0700

    Add files via upload

--- teacher.php
+++ teacher.php
@@ -18,6 +18,7 @@
 
   <!-- 
 	//////////////////////////////////////////////////////
+
 	FREE HTML5 TEMPLATE 
 	DESIGNED & DEVELOPED by FreeHTML5.co
 		
@@ -25,6 +26,7 @@
 	Email: 			info@freehtml5.co
 	Twitter: 		http://twitter.com/fh5co
 	Facebook: 		https://www.facebook.com/fh5co
+
 	//////////////////////////////////////////////////////
 	 -->
 
